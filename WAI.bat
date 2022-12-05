1e8049c37d583af7bbf92a803551a19a0aa8c1a4
Rune <38292254+rune004@users.noreply.github.com>
2022-12-05 23:52:06 +0100

    Added New Installers
    Added
    
    Chrome
    Office

--- WAI.bat
+++ WAI.bat
@@ -10,10 +10,10 @@ echo Select a task:
 echo ===========================
 echo ---------------------------
 echo 1) Install Microsoft Office
-echo 2) Install Papercut
+echo 2) Install Papercut (Missing EXE)
 echo 3) Install Wordmat
 echo 4) Install Chrome
-echo 5) Install appwriter
+echo 5) Install appwriter (Missing EXE)
 echo ---------------------------
 set /p op=Type option:
 if "%op%"=="" goto begin
@@ -42,7 +42,19 @@ goto begin
 
 ::------------------------------
 :op2
-call "c:\Users\%Username%\Desktop\BAWR\BAWR-functions\move-reset-delete.bat"
+call "c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\"
+ECHO.
+echo Please Wait...
+timeout /t 3 /nobreak > nul
+ECHO.
+pause
+goto begin
+::------------------------------
+
+::------------------------------
+:op3
+curl.exe -o WordMat.exe https://github.com/Eduap-com/WordMat/releases/download/v.1.25/WordMat125-2.exe
+call "c:\Users\%Username%\Downloads\WordMat.exe"
 ECHO.
 echo Please Wait...
 timeout /t 3 /nobreak > nul
@@ -53,100 +65,26 @@ goto begin
 
 ::------------------------------
 :op4
-CLS
-echo .
-timeout /t 1 /nobreak > nul
-echo ..
-timeout /t 1 /nobreak > nul
-echo ...
-timeout /t 1 /nobreak > nul
-echo ....
-timeout /t 1 /nobreak > nul
-echo .....
-timeout /t 1 /nobreak > nul
-ECHO.
-echo Thank You For Using A Rune Prodution Product
-ECHO.
-timeout /t 1 /nobreak > nul
-echo .....
-timeout /t 1 /nobreak > nul
-echo ....
-timeout /t 1 /nobreak > nul
-echo ...
-timeout /t 1 /nobreak > nul
-echo ..
-timeout /t 1 /nobreak > nul
-echo .
-goto exit
-::------------------------------
-
-::------------------------------
-:op3
-:more
-CLS
-echo More Options:
-echo ===========================
-echo ---------------------------
-echo 1) Create GodMode Folders
-echo 2) Test 2
-echo 3) Enable Windows Admin "SuperUser"
-echo 4) Disable Windows Admin "SuperUser"
-echo 5) Exit More Options
-echo ---------------------------
-set /p op=Type option:
-if "%op%"=="1" goto more1
-if "%op%"=="2" goto more2
-if "%op%"=="3" goto more3
-if "%op%"=="4" goto more4
-if "%op%"=="5" goto more5
-::------------------------------
-
-::------------------------------
-::User input
-echo Please Pick an option:
-goto more
-::------------------------------
-
-::------------------------------
-:more1
-call "c:\Users\%Username%\Desktop\BAWR\BAWR-functions\make-godmode-folders.bat"
-ECHO.
-echo Successfully Created GodMode Folders In The BAWR-Functions Folder On Your Desktop.
-pause
-goto more
-::------------------------------
-
-::------------------------------
-:more2
-echo test opt 1
+call "c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\ChromeSetup.exe"
 ECHO.
 echo Please Wait...
 timeout /t 3 /nobreak > nul
 ECHO.
 pause
-goto more
+goto begin
+
+goto begin
 ::------------------------------
 
 ::------------------------------
-:more3
-call "c:\Users\%Username%\Desktop\BAWR\BAWR-functions\enable-superuser.bat
+:op5
+call "c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\"
+ECHO.
+echo Please Wait...
+timeout /t 3 /nobreak > nul
+ECHO.
 pause
 goto begin
-::------------------------------
-
-::------------------------------
-:more4
-call "c:\Users\%Username%\Desktop\BAWR\BAWR-functions\disable-superuser.bat
-pause
-goto begin
-::------------------------------
-
-::------------------------------
-:more5
-
-CLS
-goto begin
-::------------------------------
 
 goto begin
 ::------------------------------
