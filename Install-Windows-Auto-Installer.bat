e478194a53c091cb2c1a9223aaa06ae26414089b
Rune <38292254+rune004@users.noreply.github.com>
2022-12-06 12:21:47 +0100

    Update Install-Windows-Auto-Installer.bat

--- Install-Windows-Auto-Installer.bat
+++ Install-Windows-Auto-Installer.bat
@@ -1,4 +1,5 @@
-move c:\Users\%Username%\Downloads\Windows-Auto-Installer-main.zip\Windows-Auto-Installer-main c:\Users\%Username%\Desktop\
+move c:\Users\%Username%\Downloads\Windows-Auto-Installer-main.zip c:\Users\%Username%\Desktop\Windows-Auto-Installer-main.zip
+unzip.exe Windows-Auto-Installer-main.zip
 echo Please Wait...
 timeout /t 3 /nobreak > nul
-@RD /S /Q "c:\Users\%Username%\Desktop\Windows-Auto-Installer-main.zip\"
+@RD /S /Q "c:\Users\%Username%\Downloads\Windows-Auto-Installer-main.zip"
