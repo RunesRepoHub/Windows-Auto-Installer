adb3dc936afe271bd2fb4451d23c5a7ecd2fbcbf
Rune <38292254+rune004@users.noreply.github.com>
2022-12-06 04:31:41 +0100

    Update WAI.bat

--- WAI.bat
+++ WAI.bat
@@ -6,12 +6,15 @@ prompt $T $B $P$G && title Rune Prodution's WAI Version 1.1.0 && color 2
 :: This is the selecting menu
 :begin
 CLS
+echo ====================================
+echo Rune Prodution's WAI Version 1.1.0
+echo ====================================
 echo Select a task:
-echo ===========================
-echo ---------------------------
+echo ====================================
+echo ------------------------------------
 echo 1) Run Windows Auto Installer
 echo 2) Exit Window Auto Installer
-echo ---------------------------
+echo ------------------------------------
 set /p op=Type option:
 if "%op%"=="" goto begin
 if "%op%"=="1" goto op1
@@ -33,26 +36,12 @@ ECHO.
 pause
 
 
-
-
-call "c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\"
-ECHO.
-echo Please Wait...
-timeout /t 3 /nobreak > nul
-ECHO.
-pause
-
-
-
-
 call "c:\Users\%Username%\Downloads\WordMat.exe"
 ECHO.
 echo Please Wait...
 timeout /t 3 /nobreak > nul
 ECHO.
 pause
-
-
 
 
 call "c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\ChromeSetup.exe"
