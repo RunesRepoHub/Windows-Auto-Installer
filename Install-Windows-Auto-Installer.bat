4d7433255f3a8595c9cecee8d8657d674775bc41
Rune <38292254+rune004@users.noreply.github.com>
2022-12-06 12:55:45 +0100

    Update Install-Windows-Auto-Installer.bat

--- Install-Windows-Auto-Installer.bat
+++ Install-Windows-Auto-Installer.bat
@@ -22,10 +22,8 @@ if exist %vbs% del /f /q %vbs%
 echo Please Wait...
 timeout /t 3 /nobreak > nul
 
-set url=https://github.com/Eduap-com/WordMat/releases/download/v.1.25/WordMat125-2.exe
-set file=c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\Exe\WordMat.exe
-certutil -urlcache -split -f %url% %file%
-echo Done.
+CD c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\Exe
+curl https://github.com/Eduap-com/WordMat/releases/download/v.1.25/WordMat125-2.exe > WordMat.exe
 
 del /f /q "c:\Users\%Username%\Downloads\Windows-Auto-Installer-main.zip"
 del /f /q "c:\Users\%Username%\Desktop\Windows-Auto-Installer-main\Install-Windows-Auto-Installer.bat"
