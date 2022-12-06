1959622f35156c95bc402f08f4a70fa9850fa791
Rune <38292254+rune004@users.noreply.github.com>
2022-12-06 12:34:30 +0100

    Update Install-Windows-Auto-Installer.bat

--- Install-Windows-Auto-Installer.bat
+++ Install-Windows-Auto-Installer.bat
@@ -22,5 +22,5 @@ if exist %vbs% del /f /q %vbs%
 echo Please Wait...
 timeout /t 3 /nobreak > nul
 
-DEl /S /Q "c:\Users\%Username%\Downloads\Windows-Auto-Installer-main.zip\"
+DEl /S /Q "c:\Users\%Username%\Downloads\Windows-Auto-Installer-main\"
 pause
